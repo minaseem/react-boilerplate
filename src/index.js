fff34959b88caf690df4e46829f4b0a6d3b72671
Imamudin Naseem <naseem1992@gmail.com>
2017-08-22 14:27:48 +0530

    reverting changes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './components/app'
 import {AppContainer} from 'react-hot-loader'
-import rwc from 'rwc';
-import * as U from 'fp-util';
-console.log(rwc);
+
 const render = Component => {
     ReactDOM.render(
         <AppContainer><Component /></AppContainer>,
